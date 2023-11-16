3009f0fe5d1636fdb7985a803edb2565281211e4
“邓浩” <1051766345@qq.com>
2023-11-16 17:01:05 +0800

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:vue/vue3-essential",
+    ],
+    "overrides": [
+    ],
+    "parser": "vue-eslint-parser",
+    "parserOptions": {
+        "parser": "@typescript-eslint/parser",
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue",
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
+}
---
+++ .prettierrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  bracketSpacing: true,
+  singleQuote: false,
+  arrowParens: "avoid",
+  trailingComma: "none",
+  // 关闭文件末尾必须有一行空行的格式化规则
+  endOfLine: "auto"
+};
